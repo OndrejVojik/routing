d84f586874f622b0e4a339da8b5cc101672794f5
OndrejVojik <vojik.cz@gmail.com>
2024-09-14 13:48:49 +0200

    8

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -28,7 +28,7 @@ export default function HostVans() {
 
     return (
         <section>
-            <h1 className="host-vans-title">Your listed vans</h1>
+            <h1 className="host-vans-title">Ahoj tati!!!</h1>
             <div className="host-vans-list">
                 {
                     vans.length > 0 ? (
